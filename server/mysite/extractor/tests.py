e241d15cb1d01daaa68df2da11c98398d0d54ccd
Nicholas Cheung <nichc74@gmail.com>
2022-10-17 03:31:40 -0700

    Adding util functions: generate_snippet() and extract_dates(WIP) along with corresponding unit tests

--- server/mysite/extractor/tests.py
+++ server/mysite/extractor/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from extractor.utils import generate_snippet, extract_dates
 
-# Create your tests here.
+
+class TestGenerateDateSnippet(TestCase):
+    def test_generate_basic_snippet(self):
+        # Test with
+        date_index = 3
+        extracted_text = ["Some", "with", "Nicholas", "10/10/2021", "for", "text"]
+        self.assertEqual("with Nicholas 10/10/2021 for text", generate_snippet(date_index, extracted_text))
+
+        # test JUST the date
+        self.assertEqual("10/10/2021", generate_snippet(0, ["10/10/2021"]))
+
+    def test_generate_snippet_with_date_at_beginning(self):
+        date_index = 0
+        # Test with full extracted_txt
+        avge_len_extracted_txt = ["10/10/2021", "for", "text", "Nicholas", "some_more"]
+        self.assertEqual("10/10/2021 for text Nicholas some_more", generate_snippet(date_index, avge_len_extracted_txt))
+
+        # Test with smaller extracted_txt
+        blw_len_extracted_txt = ["10/10/2021", "for", "text"]
+        self.assertEqual("10/10/2021 for text", generate_snippet(date_index, blw_len_extracted_txt))
+
+    def test_generate_snippet_with_date_at_end(self):
+        # Test with full snippet
+        date_avg_len_index = 4
+        avge_len_extracted_text = ["some_more", "for", "text", "Nicholas", "10/10/2021"]
+        self.assertEqual("some_more for text Nicholas 10/10/2021", generate_snippet(date_avg_len_index, avge_len_extracted_text))
+
+        # Test with smaller snippet
+        date_blw_len_index = 2
+        blw_len_extracted_text = ["text", "Nicholas", "10/10/2021"]
+        self.assertEqual("text Nicholas 10/10/2021", generate_snippet(date_blw_len_index, blw_len_extracted_text))
+
+
+class TestExtractDates(TestCase):
+    # IN PROGRESS UNIT TEST SUITE
+    def test_initial_string(self):
+        test_string = "10/30/2019 Terms of Service | KTVU FOX 2 https://www .ktvu.com/terms-of-service 1/21Terms of Service Terms of Service"
+        self.assertEqual("10/30/2019 Terms of Service |", extract_dates(test_string))
+
+    def test_extracting_date_with_dashes(self):
+        test_string = "In some string I am checking for the date: 10-10-2020 and I hope it works"
+        self.assertEqual("the date: 10-10-2020 and I", extract_dates(test_string))
+
+    def test_extracting_date_with_slashes(self):
+        test_string = "In some string I am checking for the date: 10/10/2020 and I hope it works"
+        self.assertEqual("the date: 10/10/2020 and I", extract_dates(test_string))
+
+    def test_extracting_date_with_full_month_spelled_out(self):
+        test_string = "In some string I am checking for the date: October 10, 2020 and I hope it works"
+        self.assertEqual("the date: October 10, 2020 and I", extract_dates(test_string))
+
+    def test_extracting_date_with_partial_month_spelled_out(self):
+        test_string = "In some string I am checking for the date: Oct 10, 2020 and I hope it works"
+        self.assertEqual("the date: Oct 10, 2020 and I", extract_dates(test_string))
+
+    def test_extracting_date_with_full_month_spelled_out_minus_space(self):
+        test_string = "In some string I am checking for the date: Octorber 10,2020 and I hope it works"
+        self.assertEqual("the date: Octorber 10,2020 and I", extract_dates(test_string))
---
+++ server/mysite/extractor/utils.py
@@ -0,0 +1,43 @@
+import re
+
+LENGTH_OF_SNIPPET = 4
+
+regex_date_with_slashes = "(\d|\d{2})\/(\d|\d{2})\/(\d{4})"
+regex_date_with_dashes = "(\d|\d{2})-(\d|\d{2})-(\d{4})"
+
+def generate_snippet(index, extracted_text):
+    result = ""
+    # variable to account for length of snippet. Can be adjusted to add more words to snippet
+    if LENGTH_OF_SNIPPET > len(extracted_text):
+        result = ' '.join(extracted_text)
+    else:
+        mid_snip_offset = int(LENGTH_OF_SNIPPET / 2)
+        # Date is in middle of extracted_text
+        if (index in range(mid_snip_offset, len(extracted_text) - mid_snip_offset)):
+            # Do an add 1 at the end to account for index offset
+            result = ' '.join(extracted_text[index - mid_snip_offset:index + mid_snip_offset + 1])
+        elif index == 0:
+            result = ' '.join(extracted_text[index:index + LENGTH_OF_SNIPPET + 1])
+        elif index == len(extracted_text) - 1:
+            result = ' '.join(extracted_text[index - LENGTH_OF_SNIPPET:index + 1])
+
+    return result
+
+
+def extract_dates(input_string):
+    extracted_text = input_string.split(' ')
+    result = {}
+    for index in range(len(extracted_text)):
+        word = extracted_text[index]
+        # Have multiple regex since its easier to debug
+        if re.match(regex_date_with_slashes, word):
+            extracted_date = re.match(regex_date_with_slashes, word).string
+        elif re.match(regex_date_with_dashes, word):
+            extracted_date = re.match(regex_date_with_dashes, word).string
+
+        if extracted_date not in result:
+            result[str(extracted_date)] = {
+                "date": extracted_date,
+                "snippet": generate_snippet(index, extracted_text)
+            }
+    return result
