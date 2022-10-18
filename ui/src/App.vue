1cdd39ba54b860b2d0eab51e2740d252f91500b8
Nicholas Cheung <nichc74@gmail.com>
2022-10-17 22:21:38 -0700

    Created VueJS application with vueify. Created a basic calendar with basic events hard coded

---
+++ ui/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <div>
+        <Calendar/>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import Calendar from './components/Calendar';
+
+export default {
+  name: 'App',
+
+  components: {
+    Calendar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+</style>
