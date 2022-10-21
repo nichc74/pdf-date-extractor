9878eac8dbbd6d010e0b757f194f2e42aac54dfe
Nicholas Cheung <nichc74@gmail.com>
2022-10-20 19:13:41 -0700

    Cleaning up and linting test and constant files

--- server/mysite/extractor/constants.py
+++ server/mysite/extractor/constants.py
@@ -1,16 +1,21 @@
 LENGTH_OF_SNIPPET = 4
 
 # MM/DD/YYYY - middle endian
-regex_mid_end_with_slashes = "(\d|\d{2})\/(\d|\d{2})\/(\d{4})"
-regex_mid_end_with_dashes = "(\d|\d{2})-(\d|\d{2})-(\d{4})"
+regex_mid_end_with_slashes = "^(\d|\d{2})\/(\d|\d{2})\/(\d{4})$"
+regex_mid_end_with_dashes = "^(\d|\d{2})-(\d|\d{2})-(\d{4})$"
 
 # YYYY/MM/DD - big endian
 regex_big_end_with_slashes = "^(\d{4})\/(\d{1,2})\/(\d{1,2})$"
 regex_big_end_with_dashes = "^(\d{4})-(\d{1,2})-(\d{1,2})$"
 
 # DD MM YYYY - little endian with month spelled out
-regex_lttle_end_spelled = r"^(\d{1,2})\s((\bJanuary)|(\bFebruary)|(\bMarch)|(\bApril)|(\bMay)|(\bJune)|(\bJuly)|(\bAugust)|(\bSeptember)|(\bOctober)|(\bNovember)|(\bDecember))\s(\d{4})"
+regex_lttle_end_spelled = (r"^(\d{1,2})\s((\bJanuary)|(\bFebruary)|(\bMarch)|"
+                           r"(\bApril)|(\bMay)|(\bJune)|(\bJuly)|(\bAugust)|"
+                           r"(\bSeptember)|(\bOctober)|(\bNovember)|"
+                           r"(\bDecember))\s(\d{4})")
 
-full_month_regex = r"^((\bJanuary)|(\bFebruary)|(\bMarch)|(\bApril)|(\bMay)|(\bJune)|(\bJuly)|(\bAugust)|(\bSeptember)|(\bOctober)|(\bNovember)|(\bDecember))$"
+full_month_regex = (r"^((\bJanuary)|(\bFebruary)|(\bMarch)|(\bApril)|(\bMay)|"
+                    r"(\bJune)|(\bJuly)|(\bAugust)|(\bSeptember)|"
+                    r"(\bOctober)|(\bNovember)|(\bDecember))$")
 
 harbour_blue_hex = '#2f5a89'
--- server/mysite/extractor/tests.py
+++ server/mysite/extractor/tests.py
@@ -10,17 +10,18 @@ from extractor.utils import (
 
 class TestEncapsulatePossibleDate(TestCase):
     def test_base_spelled_out_date(self):
-        extracted_text = ["and", "then", "17", "October", "2022", "with", "some"]
+        extracted_text = ["and", "then", "17",
+                          "October", "2022", "with", "some"]
         month_index = 3
         result = encap_poss_date(month_index, extracted_text)
         self.assertEqual(result, "17 October 2022")
-    
+
     def test_spelled_out_date_begin_str(self):
         extracted_text = ["17", "October", "2022", "with", "some"]
         month_index = 1
         result = encap_poss_date(month_index, extracted_text)
         self.assertEqual(result, "17 October 2022")
-    
+
     def test_spelled_out_date_end_str(self):
         extracted_text = ["with", "some", "17", "October", "2022"]
         month_index = 3
@@ -32,8 +33,10 @@ class TestGenerateDateSnippet(TestCase):
     def test_generate_basic_snippet(self):
         # Test with
         date_index = 3
-        extracted_text = ["Some", "with", "Nicholas", "10/10/2021", "for", "text"]
-        self.assertEqual("...with Nicholas 10/10/2021 for text...", generate_snippet(date_index, extracted_text))
+        extracted_text = ["Some", "with",
+                          "Nicholas", "10/10/2021", "for", "text"]
+        self.assertEqual("...with Nicholas 10/10/2021 for text...",
+                         generate_snippet(date_index, extracted_text))
 
         # test JUST the date
         self.assertEqual("10/10/2021", generate_snippet(0, ["10/10/2021"]))
@@ -41,23 +44,29 @@ class TestGenerateDateSnippet(TestCase):
     def test_generate_snippet_with_date_at_beginning(self):
         date_index = 0
         # Test with full extracted_txt
-        avge_len_extracted_txt = ["10/10/2021", "for", "text", "Nicholas", "some_more"]
-        self.assertEqual("10/10/2021 for text Nicholas some_more...", generate_snippet(date_index, avge_len_extracted_txt))
+        avge_len_extracted_txt = ["10/10/2021",
+                                  "for", "text", "Nicholas", "some_more"]
+        self.assertEqual("10/10/2021 for text Nicholas some_more...",
+                         generate_snippet(date_index, avge_len_extracted_txt))
 
         # Test with smaller extracted_txt
         blw_len_extracted_txt = ["10/10/2021", "for", "text"]
-        self.assertEqual("10/10/2021 for text", generate_snippet(date_index, blw_len_extracted_txt))
+        self.assertEqual("10/10/2021 for text",
+                         generate_snippet(date_index, blw_len_extracted_txt))
 
     def test_generate_snippet_with_date_at_end(self):
         # Test with full snippet
         date_avg_len_index = 4
-        avge_len_extracted_text = ["some_more", "for", "text", "Nicholas", "10/10/2021"]
-        self.assertEqual("...some_more for text Nicholas 10/10/2021", generate_snippet(date_avg_len_index, avge_len_extracted_text))
+        avg_len_extrted = ["some_more",
+                                   "for", "text", "Nicholas", "10/10/2021"]
+        self.assertEqual("...some_more for text Nicholas 10/10/2021",
+                         generate_snippet(date_avg_len_index, avg_len_extrted))
 
         # Test with smaller snippet
         date_blw_len_index = 2
-        blw_len_extracted_text = ["text", "Nicholas", "10/10/2021"]
-        self.assertEqual("text Nicholas 10/10/2021", generate_snippet(date_blw_len_index, blw_len_extracted_text))
+        blw_len_extrted = ["text", "Nicholas", "10/10/2021"]
+        self.assertEqual("text Nicholas 10/10/2021",
+                         generate_snippet(date_blw_len_index, blw_len_extrted))
 
 
 class TestExtractDates(TestCase):
@@ -65,9 +74,10 @@ class TestExtractDates(TestCase):
         self.file_name = 'testFileName.pdf'
         self.test_file_path = 'some_file_path'
 
-    # IN PROGRESS UNIT TEST SUITE
     def test_initial_string(self):
-        test_string = "10/30/2019 Terms of Service | KTVU FOX 2 https://www .ktvu.com/terms-of-service 1/21Terms of Service Terms of Service"
+        test_string = ("10/30/2019 Terms of Service | KTVU FOX 2 "
+                       "https://www .ktvu.com/terms-of-service 1/21Terms"
+                       " of Service Terms of Service")
         result = {}
         extract_dates(self.file_name, test_string, result, self.test_file_path)
         expected_result = {
@@ -106,7 +116,8 @@ class TestExtractDates(TestCase):
         self.assertEqual(expected_result, result)
 
     def test_multiple_dates_in_text(self):
-        test_string = "checking for the date: 10/10/2020 and some other 10/20/2020 I hope it works"
+        test_string = ("checking for the date: 10/10/2020 and some"
+                       "other 10/20/2020 I hope it works")
         result = {}
         extract_dates(self.file_name, test_string, result, self.test_file_path)
         expected_result = {
@@ -122,15 +133,17 @@ class TestExtractDates(TestCase):
             }
         }
         self.assertEqual(expected_result, result)
-    
+
     def test_multiple_snippets_under_one_date(self):
-        test_string = "checking for the date: 10/10/2020 and some other 10/10/2020 I hope it works"
+        test_string = ("checking for the date: 10/10/2020 and "
+                       "some other 10/10/2020 I hope it works")
         result = {}
         extract_dates(self.file_name, test_string, result, self.test_file_path)
         expected_result = {
             "testFileName.pdf_[tmp]_2020-10-10": {
                 "date": "2020-10-10",
-                "snippet": '...the date: 10/10/2020 and some...\n...some other 10/10/2020 I hope...',
+                "snippet": ("...the date: 10/10/2020 and some...\n"
+                            "...some other 10/10/2020 I hope..."),
                 "path": "some_file_path"
             },
         }
@@ -150,7 +163,9 @@ class TestExtractDates(TestCase):
         self.assertEqual(expected_result, result)
 
     def test_extract_big_endian_date_with_slashes(self):
-        test_string = "2019/10/30 Terms of Service | KTVU FOX 2 https://www .ktvu.com/terms-of-service 1/21Terms of Service Terms of Service"
+        test_string = ("2019/10/30 Terms of Service | KTVU FOX 2 "
+                       "https://www .ktvu.com/terms-of-service 1/21Terms "
+                       "of Service Terms of Service")
         result = {}
         extract_dates(self.file_name, test_string, result, self.test_file_path)
         expected_result = {
@@ -163,7 +178,9 @@ class TestExtractDates(TestCase):
         self.assertEqual(expected_result, result)
 
     def test_extract_big_endian_date_with_dashes(self):
-        test_string = "2019-10-30 Terms of Service | KTVU FOX 2 https://www .ktvu.com/terms-of-service 1/21Terms of Service Terms of Service"
+        test_string = ("2019-10-30 Terms of Service | KTVU FOX 2 "
+                       "https://www .ktvu.com/terms-of-service 1/21Terms "
+                       "of Service Terms of Service")
         result = {}
         extract_dates(self.file_name, test_string, result, self.test_file_path)
         expected_result = {
@@ -177,27 +194,27 @@ class TestExtractDates(TestCase):
 
     @skip("Skipping for now...")
     def test_extracting_date_with_full_month_spelled_out(self):
-        test_string = "checking for the date: October 10, 2020 and I hope it works"
+        test_string = "checking for date: October 10, 2020 and I hope it works"
         result = {}
         extract_dates(self.file_name, test_string, result, self.test_file_path)
         self.assertEqual("the date: October 10, 2020 and I", result)
 
     @skip("Skipping for now...")
     def test_extracting_date_with_partial_month_spelled_out(self):
-        test_string = "checking for the date: Oct 10, 2020 and I hope it works"
+        test_string = "checking for date: Oct 10, 2020 and I hope it works"
         result = {}
         extract_dates(self.file_name, test_string, result, self.test_file_path)
         self.assertEqual("the date: Oct 10, 2020 and I", result)
 
     @skip("Skipping for now...")
     def test_extracting_date_with_full_month_spelled_out_minus_space(self):
-        test_string = "checking for the date: Octorber 10,2020 and I hope it works"
+        test_string = "checking for date: Octorber 10,2020 and I hope it works"
         result = {}
         extract_dates(self.file_name, test_string, result, self.test_file_path)
         self.assertEqual("the date: Octorber 10,2020 and I", result)
 
     def test_extracting_day_month_year_spelled_out(self):
-        test_string = "checking for the date: 12 November 2022 and I hope it works"
+        test_string = "checking for date: 12 November 2022 and I hope it works"
         result = {}
         extract_dates(self.file_name, test_string, result, self.test_file_path)
         expected_result = {
@@ -224,7 +241,7 @@ class TestMassageExtractedDates(TestCase):
                 "path": "some_file_path"
             }
         }
-    
+
     def test_simple_massage(self):
         result = massage_calendar_dates(self.extracted_dates)
         expected_result = [
@@ -245,5 +262,3 @@ class TestMassageExtractedDates(TestCase):
             }
         ]
         self.assertEqual(expected_result, result)
-    
-
