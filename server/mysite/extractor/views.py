50bf352d63447db0ada4ea41ba62e05cae8f8d1e
Nicholas Cheung <nichc74@gmail.com>
2022-10-20 21:10:19 -0700

    Adding a try/except to post view

--- server/mysite/extractor/views.py
+++ server/mysite/extractor/views.py
@@ -1,4 +1,5 @@
 import io
+import logging
 
 from django.http import JsonResponse
 from django.views import View
@@ -10,6 +11,7 @@ from django.core.files.storage import FileSystemStorage
 from PyPDF2 import PdfFileReader
 from extractor.utils import extract_dates, norm_cal_data
 
+logger = logging.getLogger(__name__)
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ExtractorView(View):
@@ -21,25 +23,28 @@ class ExtractorView(View):
         # Process individual pdf docs
         for pdf_key in pdf_keys:
             # Setup PDF Reader
-            pdf = pdf_files_uploaded[pdf_key]
-            pdfFileObj = pdf.read()
-            pdfReader = PdfFileReader(io.BytesIO(pdfFileObj))
+            try:
+                pdf = pdf_files_uploaded[pdf_key]
+                pdfFileObj = pdf.read()
+                pdfReader = PdfFileReader(io.BytesIO(pdfFileObj))
 
-            # Store in FileSystemStorage
-            file_storage = FileSystemStorage()
-            file_storage_save = file_storage.save(pdf.name, pdf)
-            file_url = request.build_absolute_uri(
-                '/')[:-1] + file_storage.url(file_storage_save)
+                # Store in FileSystemStorage
+                file_storage = FileSystemStorage()
+                file_storage_save = file_storage.save(pdf.name, pdf)
+                file_url = request.build_absolute_uri(
+                    '/')[:-1] + file_storage.url(file_storage_save)
 
-            dates_for_curr_pdf = {}
-            # Process individual pdf pages
-            for i in range(pdfReader.numPages):
-                curr_page = pdfReader.pages[i].extract_text()
-                extract_dates(pdf.name, curr_page,
-                              dates_for_curr_pdf, file_url)
+                dates_for_curr_pdf = {}
+                # Process individual pdf pages
+                for i in range(pdfReader.numPages):
+                    curr_page = pdfReader.pages[i].extract_text()
+                    extract_dates(pdf.name, curr_page,
+                                  dates_for_curr_pdf, file_url)
 
-            if dates_for_curr_pdf:
-                log_of_extracted_dates.update(dates_for_curr_pdf)
+                if dates_for_curr_pdf:
+                    log_of_extracted_dates.update(dates_for_curr_pdf)
+            except:
+                logger.error("Something went wrong while processing your PDFs")
 
         return_data = {
             'message': 'You are pinging the proper view',
